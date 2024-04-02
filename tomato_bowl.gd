a6deefba1cdeefbdd1b15e96e06ad0feaa256d2d
williamrobinsonbsu <143113909+williamrobinsonbsu@users.noreply.github.com>
2024-04-02 13:36:37 -0400

    added ingredient bowls

--- tomato_bowl.gd
+++ tomato_bowl.gd
@@ -3,6 +3,6 @@ extends StaticBody3D
 var scene = preload("res://ingredients/tomato_whole.tscn")
 
 func pick_item():
-	var tomato = scene.instantiate()
-	add_child(tomato)
-	return tomato
+	var lettuce = scene.instantiate()
+	add_child(lettuce)
+	return lettuce
