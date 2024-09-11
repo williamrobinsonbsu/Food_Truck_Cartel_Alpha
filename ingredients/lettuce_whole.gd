68f6f05fa67c2efb4aae2f68d91476be4bd5a83f
williamrobinsonbsu <william.robinson@bsu.edu>
2024-09-11 13:48:54 -0400

    Refactor textures and models

--- ingredients/lettuce_whole.gd
+++ ingredients/lettuce_whole.gd
@@ -1,13 +1,5 @@
 extends PickedIngredient
 
-var cutted_lettuce = false
-
-func cut_lettuce():
-	if cutted_lettuce == false:
-		$lettuce_whole.hide()
-		$lettuce_slice.show()
-		cutted_lettuce = true
-		
 func name():
 	return "lettuce"
 
