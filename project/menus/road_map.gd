035dcf01634e6b9f86116df2f7d24ab0fc73de0a
SaltyTea4924 <tanner.bauserman@bsu.edu>
2024-10-11 12:16:55 -0400

    Add new roadmap place placeholder pin

--- project/menus/road_map.gd
+++ project/menus/road_map.gd
@@ -13,10 +13,25 @@ func _process(delta):
 func _on_beach_pressed():
 	get_tree().change_scene_to_file("res://levels/beach.tscn")
 
+func _on_beach_mouse_entered():
+	$placeholder.position = $Beach.position
+	$placeholder.visible = true
+
+func _on_beach_mouse_exited():
+	$placeholder.visible = false
 
 func _on_rave_pressed():
 	get_tree().change_scene_to_file("res://levels/beach.tscn")
 
+func _on_rave_mouse_entered():
+	$placeholder.position = $Rave.position
+	$placeholder.visible = true
+
+func _on_rave_mouse_exited():
+	$placeholder.visible = false
+
 
 func _on_return_pressed():
 	get_tree().change_scene_to_file("res://menus/start_menu.tscn")
+
+
