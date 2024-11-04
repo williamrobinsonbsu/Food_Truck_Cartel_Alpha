bb9815a6af3a045463d820886ea8a81c995f3ba0
williamrobinsonbsu <william.robinson@bsu.edu>
2024-11-04 13:34:41 -0500

    Add ingredient despawning

--- project/ingredients/pickable_ingredient.gd
+++ project/ingredients/pickable_ingredient.gd
@@ -1,10 +1,26 @@
 class_name PickedIngredient extends RigidBody3D
 
+
+var picked_status
+
+@onready var timer_path := preload("res://ingredients/queue_free_timer.tscn")
+var timer: Timer
+
+
 func picked(pick):
 	if pick == true:
+		if timer == null:
+			timer = timer_path.instantiate()
+			add_child(timer)
 		set_collision_layer_value(1, false)
 		set_collision_mask_value(1, false)
+		timer.paused = true
 	elif pick == false:
 		set_collision_layer_value(1, true)
 		set_collision_mask_value(1, true)
+		timer.paused = false
+		timer.start(10)
+		timer.timeout.connect(func():
+				self.queue_free())
+		
 		
