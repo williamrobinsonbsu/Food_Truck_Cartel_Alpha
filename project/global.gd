8eadb11492230df8d391aeb1c20aa61978d69cd2
williamrobinsonbsu <william.robinson@bsu.edu>
2024-11-07 11:39:47 -0500

    Telemetryy

--- project/global.gd
+++ project/global.gd
@@ -1,6 +1,11 @@
 extends Node
 
 
+var data
+var data_array: Array = ["Pickups", "Drops", "Successful Submissions", "Failed Submissions", "Plate Clears", "Crouches", "Shutters", "Pauses"]
+var index := 0
+
+
 @export var pick_up: int = 0
 @export var drop: int = 0
 @export var succ_submit: int = 0
@@ -14,9 +19,12 @@ extends Node
 func save_data():
 	var log_file = "user://" + Time.get_date_string_from_system() + ".csv"
 	var file = FileAccess.open(log_file, FileAccess.WRITE)
+	var numeric_data = [pick_up, drop, succ_submit, fai_submit, clear, crouch, shutter, pause]
 	print(log_file)
 	
-	var data = PackedStringArray(["Pick-ups:", str(pick_up)])
 	
-	file.store_csv_line(data)
+	while index < 8:
+		data = PackedStringArray([data_array[index], str(numeric_data[index])])
+		file.store_csv_line(data)
+		index += 1
 	file = null
