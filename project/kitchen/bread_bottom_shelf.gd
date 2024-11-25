5f5c9dfee3e985d3c556beb74b56c3c07fa89576
Jamison8 <jamisontuckerwork@gmail.com>
2024-11-25 17:17:53 -0500

    Add text tips to buns

--- project/kitchen/bread_bottom_shelf.gd
+++ project/kitchen/bread_bottom_shelf.gd
@@ -2,7 +2,17 @@ extends StaticBody3D
 
 var scene = preload("res://ingredients/bottom_bun.tscn")
 
+func _ready():
+	$ActionLabelBottomBun.hide()
+
 func pick_item():
 	var soda = scene.instantiate()
 	add_child(soda)
 	return soda
+
+func hover_name():
+	$ActionLabelBottomBun/LabelBottomBunTimer.start()
+	$ActionLabelBottomBun.show()
+
+func _on_label_bottom_bun_timer_timeout():
+	$ActionLabelBottomBun.hide()
--- project/kitchen/top_bun_shelf.gd
+++ project/kitchen/top_bun_shelf.gd
@@ -2,7 +2,17 @@ extends StaticBody3D
 
 var scene = preload("res://ingredients/top_bun.tscn")
 
+func _ready():
+	$ActionLabelTopBun.hide()
+
 func pick_item():
 	var soda = scene.instantiate()
 	add_child(soda)
 	return soda
+
+func hover_name():
+	$ActionLabelTopBun/LabelTopBunTimer.start()
+	$ActionLabelTopBun.show()
+
+func _on_label_top_bun_timer_timeout():
+	$ActionLabelTopBun.hide()
