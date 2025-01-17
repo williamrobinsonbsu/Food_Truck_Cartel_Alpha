3d4eeda46cc529d2b234b38783d4ee2696d5cb37
Robinson <william.robinson@bsu.edu>
2025-01-17 12:23:28 -0500

    Add menu button sound

---
+++ project/autoloads/UISoundPlayer.gd
@@ -0,0 +1,31 @@
+extends Node
+
+
+func _ready():
+	connect_buttons(get_tree().root)
+	get_tree().node_added.connect(func(node):
+			if node is Button:
+				connect_to_button(node))
+
+
+func connect_buttons(root):
+	for child in root.get_children():
+		if child is Button:
+			connect_to_button(child)
+		connect_buttons(child)
+		
+
+func connect_to_button(button: Button):
+	button.pressed.connect(func():
+			play_button_sound())
+
+func play_button_sound():
+	var audio_stream_player := AudioStreamPlayer.new()
+	audio_stream_player.stream = load("res://autoloads/button_click.wav")
+	audio_stream_player.bus = "Sound"
+	audio_stream_player.volume_db = linear_to_db(10)
+	get_parent().add_child(audio_stream_player)
+	audio_stream_player.play()
+	audio_stream_player.finished.connect(func():
+		audio_stream_player.queue_free()
+	)
