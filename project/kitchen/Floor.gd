7491246f80ffb7488b22675e71f2bd854a797665
Jamison8 <111749670+jamisontucker@users.noreply.github.com>
2025-01-24 12:46:33 -0500

    change floor collision to on body entered

--- project/kitchen/Floor.gd
+++ project/kitchen/Floor.gd
@@ -1,15 +1,10 @@
 extends StaticBody3D
 
-
+signal on_floor
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_area_3d_area_entered(area):
-	pass # Replace with function body.
+func _on_area_3d_body_entered(body):
+	on_floor.emit()
