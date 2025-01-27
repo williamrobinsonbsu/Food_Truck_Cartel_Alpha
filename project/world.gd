c3d42b28fecbb3b260f89ea78ede151a6dc5cd09
Jamison8 <111749670+jamisontucker@users.noreply.github.com>
2025-01-27 13:11:29 -0500

    Add slice_name to queue free on floor

--- project/world.gd
+++ project/world.gd
@@ -266,5 +266,5 @@ func _on_cop_timer_timeout():
 
 
 func _on_area_3d_body_entered(body):
-	if body.has_method("name"):
+	if body.has_method("name") or body.has_method("slice_name"):
 		body.queue_free()
