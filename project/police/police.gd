fc52d4e08f6263f029aa9e2ef7b9c4ea5b1baf3f
Robinson <william.robinson@bsu.edu>
2025-02-03 13:32:28 -0500

    Add area 51 guard

--- project/police/police.gd
+++ project/police/police.gd
@@ -1,8 +1,16 @@
 extends StaticBody3D
 
+
+func _ready():
+	if Global.curr_level == "Area51":
+		$PoliceFront.texture = load("res://police/area_51_guard_npc_front.png")
+		$PoliceSide.texture = load("res://police/area_51_guard_npc_side.png")
+
+
 func despawn():
 	$".".queue_free()
-	
+
+
 func catch():
 	$PoliceFront.show()
 	$PoliceSide.hide()
