acec089bd1e641e7abced89c2c1668a007a637cf
SaltyTea4924 <tanner.bauserman@bsu.edu>
2025-02-10 13:19:48 -0500

    Fixed Hot dog problems

--- project/kitchen/dog_machine.gd
+++ project/kitchen/dog_machine.gd
@@ -6,7 +6,6 @@ func use(ingredient):
 	if ingredient.has_method("name") and ingredient.name() == "hot_dog_raw":
 		$Hot_dog_raw.show()
 		ingredient.audio_play_grill()
-		$Hot_dog_raw.set_collision_layer_value(1, false)
 		ingredient.queue_free()
 		await get_tree().create_timer(5).timeout
 		$Hot_dog_raw.hide()
