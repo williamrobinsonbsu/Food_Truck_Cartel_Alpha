0eafb7089010f956b6b633ddd8e5162b771b9b8e
SaltyTea4924 <tanner.bauserman@bsu.edu>
2025-02-12 12:22:18 -0500

    Add burn effect for grill

--- project/ingredients/patty.gd
+++ project/ingredients/patty.gd
@@ -1,4 +1,13 @@
 extends PickedIngredient
 
+
+var smoke := preload("res://ingredients/smoke_effect/smoke.tscn")
+var is_smoking = false
 func name():
 	return "patty"
+
+
+func add_smoke():
+	var smoking = smoke.instantiate()
+	add_child(smoking)
+	smoking = smoke.instantiate()
--- project/kitchen/grill.gd
+++ project/kitchen/grill.gd
@@ -2,6 +2,16 @@ extends StaticBody3D
 
 var scene = preload("res://ingredients/patty.tscn")
 var cooking := false
+@onready var timer_path := preload("res://ingredients/queue_free_timer.tscn")
+var timer: Timer
+var burn_timer : Timer
+
+func _ready():
+	timer = timer_path.instantiate()
+	add_child(timer)
+	burn_timer = timer_path.instantiate()
+	add_child(burn_timer)
+	#timer.paused = true
 
 func use(ingredient):
 	if ingredient.has_method("name") and ingredient.name() == "patty_raw" and cooking == false:
@@ -25,5 +35,33 @@ func use(ingredient):
 
 
 func _on_grill_top_body_entered(body):
+		assert(body!=null, "Argument may not be null")
 		if body.has_method("cooks") and body.cooks() == false:
 			use(body)
+		if body.has_method("name") and body.name() == "patty":
+			#body.add_smoke()
+			if body.is_smoking == false:
+				timer.paused = false
+				timer.start(1)
+				timer.timeout.connect(func():
+					if body != null:
+						body.add_smoke()
+						body.is_smoking = true
+						timer.paused = true
+						_burn(body)
+						#timer.paused = true
+						)
+			else:
+				_burn(body)
+				
+		
+func _burn(body):
+	burn_timer.paused = false
+	burn_timer.start(1)
+	burn_timer.timeout.connect(func():
+		if body != null:
+			body.queue_free())
+
+func _on_grill_top_body_exited(body):
+	timer.paused = true
+	burn_timer.paused = true
