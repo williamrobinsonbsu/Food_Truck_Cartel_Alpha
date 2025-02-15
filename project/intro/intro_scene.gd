516f996f1916533a524605988da42f75000b0920
blazetbd <blazetbd@gmail.com>
2025-02-15 11:08:59 -0500

    Another quick fix

--- project/intro/intro_scene.gd
+++ project/intro/intro_scene.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _ready():
-	var curr = DialogueManager.show_example_dialogue_balloon(load("res://intro/dialogue.dialogue"), "intro")
+	DialogueManager.show_example_dialogue_balloon(load("res://intro/dialogue.dialogue"), "intro")
 	await DialogueManager.dialogue_ended
 	self.hide()
 	get_tree().change_scene_to_file("res://levels/beach.tscn")
