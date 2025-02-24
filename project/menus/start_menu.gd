bf86b426152d9017893abbaae16dd4260fcb74b9
Robinson <william.robinson@bsu.edu>
2025-02-24 12:22:05 -0500

    Add new truck

--- project/menus/start_menu.gd
+++ project/menus/start_menu.gd
@@ -4,7 +4,6 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	MenuMusic.play_music_level()
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
